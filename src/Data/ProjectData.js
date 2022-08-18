a7ba0487eb5b3cde1731011f70e82e86c0167ecc
Joseph Arteaga <74483194+jarteaga127@users.noreply.github.com>
2022-08-18 22:38:42 +0900

    Update ProjectData.js

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,15 +1,19 @@
 import ComingSoon from "../images/DSC00808.JPG";
+import IncomeTracker from "../images/income-tracker.jpg";
+import ToDoList from "../images/todolist.jpg";
 
 export const ProjectData = [
     {
-        image: ComingSoon,
-        alt: "Coming Soon",
-        title: "Coming Soon"
+        image: ToDoList,
+        alt: "To do list",
+        title: "To Do List",
+        link: "https://codesandbox.io/s/todo-list-react-cvveb4"
     },
     {
-        image: ComingSoon,
-        alt: "Coming Soon",
-        title: "Coming Soon"
+        image: IncomeTracker,
+        alt: "Income Tracker",
+        title: "Income Tracker App",
+        link: "https://codesandbox.io/s/my-income-tracker-12n1n8"
     },
     {
         image: ComingSoon,
