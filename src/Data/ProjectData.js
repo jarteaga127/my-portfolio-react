dd75dc2485b46e049ab054540c2e02877a8f4f8a
Joseph Arteaga <74483194+jarteaga127@users.noreply.github.com>
2022-10-24 11:36:36 +0900

    Add files via upload

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,13 +1,21 @@
 import ComingSoon from "../images/DSC00808.JPG";
 import IncomeTracker from "../images/income-tracker.jpg";
 import ToDoList from "../images/todolist.jpg";
-import Pilgrime from "../images/pilgrime-screen.jpg"
+import Pilgrime from "../images/pilgrime-screen.jpg";
+import PadsInJapan from "../images/pads-in-japan.jpg";
 
 export const ProjectData = [
     {
+        image: PadsInJapan,
+        alt: "Real Estate Website",
+        category: "Web Development, Web Design",
+        title: "Pads In Japan",
+        link: "https://warm-elf-8325a8.netlify.app/"
+    },
+    {
         image: Pilgrime,
         alt: "Anime Pilgrimage Website",
-        category: "Web Development",
+        category: "Web Development, Web Design",
         title: "Pilgrime",
         link: "https://resonant-profiterole-cf0933.netlify.app/"
     },
